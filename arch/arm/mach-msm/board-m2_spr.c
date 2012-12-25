93841ba1cc475b7ad61ada855ca434fe0a5be5ed
detule <ogjoneski@gmail.com>
2012-12-25 16:20:40 -0500

    M2_SPR: board-m2spr: set_rpm_wakup_irq: this is no handled in devices-8960 via irq_wakeup=

--- arch/arm/mach-msm/board-m2_spr.c
+++ arch/arm/mach-msm/board-m2_spr.c
@@ -5459,7 +5459,9 @@ static void __init samsung_m2_spr_init(void)
 	slim_register_board_info(msm_slim_devices,
 		ARRAY_SIZE(msm_slim_devices));
 	msm8960_init_dsps();
+#if 0
 	msm_pm_set_rpm_wakeup_irq(RPM_APCC_CPU0_WAKE_UP_IRQ);
+#endif
 	change_memory_power = &msm8960_change_memory_power;
 	BUG_ON(msm_pm_boot_init(&msm_pm_boot_pdata));
 #if 0
