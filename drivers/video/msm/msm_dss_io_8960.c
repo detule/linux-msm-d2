6fc89d507f8ec244a7550699343b2b62484d13a6
detule <ogjoneski@gmail.com>
2013-01-10 13:11:52 -0500

    M2_SPR: video: no footswitch (as in 3.0) <- possibly unncessary

--- drivers/video/msm/msm_dss_io_8960.c
+++ drivers/video/msm/msm_dss_io_8960.c
@@ -609,6 +609,12 @@ void mipi_dsi_prepare_clocks(void)
 	clk_prepare(amp_pclk);
 	clk_prepare(dsi_m_pclk);
 	clk_prepare(dsi_s_pclk);
+	if (clk_set_rate(dsi_byte_div_clk, 1) < 0)	/* divided by 1 */
+		pr_err("%s: dsi_byte_div_clk - "
+			"clk_set_rate failed\n", __func__);
+	if (clk_set_rate(dsi_esc_clk, esc_byte_ratio) < 0) /* divided by esc */
+		pr_err("%s: dsi_esc_clk - "			 /* clk ratio */
+			"clk_set_rate failed\n", __func__);
 	clk_prepare(dsi_byte_div_clk);
 	clk_prepare(dsi_esc_clk);
 }
